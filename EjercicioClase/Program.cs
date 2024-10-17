5f545eb4bfd6366f3df117d6ec4daade4406ae12
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-16 22:47:31 -0600

    Add project files.

---
+++ EjercicioClase/Program.cs
@@ -0,0 +1,45 @@
+﻿using EjercicioClase;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        SuperPoder superFuerza = new SuperPoder { Nombre = "Super Fuerza", Descripcion = "Capacidad de levantar objetos muy pesados", Nivel = 9 };
+        SuperPoder MultiMillonario = new SuperPoder { Nombre = "MultiMillonario", Descripcion = "Tiene Mucho dinero y Mucha Tecnologia", Nivel = 10 };
+        SuperPoder velocidad = new SuperPoder { Nombre = "Velocidad", Descripcion = "Capacidad de moverse extremadamente rápido", Nivel = 8 };
+
+    
+        SuperHeroe superman = new SuperHeroe
+        {
+            Nombre = "Superman",
+            IdentidadSecreta = "Clark Kent",
+            Ciudad = "Metrópolis",
+            PuedeVolar = true,
+            SuperPoder = superFuerza
+        };
+
+        SuperHeroe batman = new SuperHeroe
+        {
+            Nombre = "Batman",
+            IdentidadSecreta = "Bruce Wayne",
+            Ciudad = "Gotham",
+            PuedeVolar = false,
+            SuperPoder = MultiMillonario
+        };
+
+        SuperHeroe flash = new SuperHeroe
+        {
+            Nombre = "Flash",
+            IdentidadSecreta = "Barry Allen",
+            Ciudad = "Central City",
+            PuedeVolar = false,
+            SuperPoder = velocidad
+        };
+
+        
+        superman.MostrarInformacion();
+        batman.MostrarInformacion();
+        flash.MostrarInformacion();
+    }
+}
---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,27 @@
+﻿namespace EjercicioClase
+{
+    
+
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public bool PuedeVolar { get; set; }
+        public SuperPoder SuperPoder { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"----------SUPER HEROES----------");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede volar: {PuedeVolar}");
+            Console.WriteLine($"Super poder: {SuperPoder.Nombre}");
+            Console.WriteLine($"Descripcion: {SuperPoder.Descripcion}");
+            Console.WriteLine($"Nivel: {SuperPoder.Nivel}");
+            Console.WriteLine();
+        }
+    }
+}
+
